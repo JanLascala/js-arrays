8e433c22a29511fc544d4f70ab8de7e40dd7e670
JanLascala <lascala.jan@gmail.com>
2025-02-12 14:23:42 +0100

    NO changes

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -46,5 +46,6 @@ console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
+
 const isTeachersEmpty = Array.isArray(teachers.length);
 console.log(isTeachersEmpty)
