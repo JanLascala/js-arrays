9dc3d0660b58f521b20c65e65eca6c48e993364c
JanLascala <lascala.jan@gmail.com>
2025-02-12 14:19:47 +0100

    Correct teacher is empty

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -46,5 +46,5 @@ console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = Array.isArray(teachers);
+const isTeachersEmpty = Array.isArray(teachers.length);
 console.log(isTeachersEmpty)
